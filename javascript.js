ca34b290e7af5012f20dc4d2368ff654c7415067
DaniSantucci <danielesantucci@hotmail.com>
2023-10-13 13:13:27 -0300

    pag categoria adicionada

--- javascript.js
+++ javascript.js
@@ -1,25 +1,52 @@
-document.getElementById('categoria-doce').addEventListener('click', function() {
-    filtrarReceitasPorCategoria('categoria-doce');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    // Função para filtrar receitas por categoria
+    function filtrarReceitasPorCategoria(categoria) {
+        var receitas = document.querySelectorAll('.receita-card');
+        receitas.forEach(function(receita) {
+            var categorias = receita.getAttribute('data-categorias').split(' ');
+            if (categorias.includes(categoria) || categoria === 'all') {
+                receita.style.display = 'block';
+            } else {
+                receita.style.display = 'none';
+            }
+        });
+    }
 
-document.getElementById('categoria-salgado').addEventListener('click', function() {
-    filtrarReceitasPorCategoria('categoria-salgado');
-});
+    // Adiciona event listeners às categorias
+    var categoriaDoce = document.getElementById('doce');
+    var categoriaSalgado = document.getElementById('salgado');
+    var categoriaPratoPrincipal = document.getElementById('prato-principal');
+    var categoriaAcompanhamento = document.getElementById('acompanhamento');
 
-document.getElementById('categoria-prato-principal').addEventListener('click', function() {
-    filtrarReceitasPorCategoria('categoria-prato-principal');
-});
-
-/* cod2*/
-
-function filtrarReceitasPorCategoria(categoria) {
-    var receitas = document.querySelectorAll('.receita-card');
-    receitas.forEach(function(receita) {
-        var categorias = receita.getAttribute('data-categorias').split(' ');
-        if (categorias.includes(categoria)) {
-            receita.style.display = 'block';
-        } else {
-            receita.style.display = 'none';
-        }
+    categoriaDoce.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        filtrarReceitasPorCategoria('doce');
     });
-}
+
+    categoriaSalgado.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        filtrarReceitasPorCategoria('salgado');
+    });
+
+    categoriaPratoPrincipal.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        filtrarReceitasPorCategoria('prato-principal');
+    });
+
+    categoriaAcompanhamento.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        filtrarReceitasPorCategoria('acompanhamento');
+    });
+
+    // Função para voltar
+    function voltar() {
+        window.history.back();
+    }
+
+    // Adiciona um event listener ao botão de voltar
+    var botaoVoltar = document.getElementById('botao-voltar');
+    botaoVoltar.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        voltar();
+    });
+});
